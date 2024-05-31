15d8fc47cb75115fec5c5f9b7d722d26ce724cae
Алексей Альхимович <alik.8383@bk.ru>
2024-05-31 14:20:20 +0300

    +

--- LB2_Alkhimovich/MainWindow.xaml.cs
+++ LB2_Alkhimovich/MainWindow.xaml.cs
@@ -91,13 +91,13 @@ namespace LB2_Alkhimovich
             return s;
         }
 
-        private long Factorial(int n)
+       /* private long Factorial(int n)
         {
             long result = 1;
             for (int i = 1; i <= n; i++)
                 result *= i;
             return result;
-        }
+        */
 
         public void Xstart_TextChanged(object sender, TextChangedEventArgs e)
         {
